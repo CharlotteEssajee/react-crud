1ad9d8530ea6a3943555d9e39e6833dd31219860
CharlotteEssajee <71342246+CharlotteEssajee@users.noreply.github.com>
2022-03-15 21:06:07 +0100

    javascript frameworks
    module 3

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import HomePage from "./components/home/HomePage";
 import LoginPage from "./components/login/LoginPage";
 import DashboardPage from "./components/dashboard/DashboardPage";
 import Nav from "./components/layout/Nav";
+import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
+import PostPage from "./components/dashboard/posts/PostPage";
+import AddPost from "./components/dashboard/posts/AddPost";
 
 function App() {
-	return (
-		<Router>
-			<Nav />
+  return (
+    <AuthProvider>
+      <Router>
+        <Nav />
 
-			<div className="container">
-				<Switch>
-					<Route exact path="/">
-						<HomePage />
-					</Route>
-					<Route path="/login">
-						<LoginPage />
-					</Route>
-					<Route path="/dashboard" exact>
-						<DashboardPage />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/dashboard" exact>
+              <DashboardPage />
+            </Route>
+            <Route path="/dashboard/posts" exact>
+              <PostPage />
+            </Route>
+            <Route path="/dashboard/posts/add">
+              <AddPost />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
